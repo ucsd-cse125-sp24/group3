276e53b85b0b257e8f7c8837f5fdd728dae7188e
Anthony Tarbinian <atar137h@gmail.com>
2024-04-10 11:51:54 -0700

    add graphics deps to nix shell
    works on X11. not working on wayland due to a missing
    "libwayland-client"

--- shell.nix
+++ shell.nix
@@ -8,5 +8,26 @@ mkShell {
     cmake
     gnumake
     gcc13
+
+    wayland
+    wayland-scanner
+    libxkbcommon
+    libffi
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    
+    glfw
+    glm
+
+    clang-tools_13
   ];
+  nativeBuildInputs = with pkgs; [
+        pkg-config
+  ];
+  shellHook = ''
+      LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+  '';
 }
