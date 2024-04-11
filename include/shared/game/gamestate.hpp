8a36c99ca6365b109a78bcacb6a2c501c285a1de
Gil Keidar <keidar.gil@gmail.com>
2024-04-11 10:53:12 -0700

    Initial bad version for the gamestate skeleton - will rewrite this code

--- include/shared/game/gamestate.hpp
+++ include/shared/game/gamestate.hpp
@@ -1,15 +1,32 @@
 #pragma once
 
-#include "shared/utilities/serializable.hpp"
+#include "GameLogic/Object.hpp"
 
 #include <string>
 
-class GameState : public Serializable {
+class GameState {
 public:
     GameState();
-    
-    std::string serialize() const override;
+
+    void updateState();
+
+    void createObject(Object * object);
+
+    Object* removeObject();
 
 private:
+    /**
+     *  Vector of all objects in this game instance.
+     */
+    std::vector<Object> objects;
 
+    /**
+     *  Time step length in milliseconds.
+     */
+    unsigned int time_step_len;
+
+    /**
+     *  Current time step (starts at 0)
+     */
+    unsigned int time_step;
 };
--- src/shared/game/gamestate.cpp
+++ src/shared/game/gamestate.cpp
@@ -2,10 +2,50 @@
 
 #include <string>
 
+/**
+ *	GameState constructor.
+ */
 GameState::GameState() {
+	//	Initialize time_step length to 30 ms
+	this->time_step_len = 30;
 
+	//	Initial time_step is 0
+	this->time_step = 0;
 }
 
-std::string GameState::serialize() const {
-    return "";
+/**
+ * Update the current game state from the previous tick to the next tick.
+ */
+void GameState::updateState() {
+	//	Update GameState from previous tick to the current one
+
+	//	Handle all events that arrived / trigger during this tick
+
+	//	Update all object positions
 }
+
+void GameState::createObject(Object * object) {
+	//	Create a new object and put it in the world (add to objects
+	//	vector)
+	this->objects.push_back(*object);
+}
+
+//	Fake main() for testing - pretend to be the server
+int main() {
+	//	Create a GameState object
+	GameState * state = new GameState();
+
+	//	Create a new object in the game world
+	Object* obj1 = new Object();
+
+
+	state->createObject(obj1);
+
+
+	//	Every time step, run the update function
+	while (1) {
+		//	Call update function
+		state->updateState();
+	}
+}
+
