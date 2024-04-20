c38797aa56e22b087cf9dc2d9a9223efa0f6342e
Tyler Lentz <tlentz@ucsd.edu>
2024-04-20 14:01:42 -0700

    refactor Server::doAccept

--- include/server/server.hpp
+++ include/server/server.hpp
@@ -19,7 +19,6 @@ using boost::asio::ip::tcp;
 class Server {
 public:
     Server(boost::asio::io_context& io_context, GameConfig config);
-    void doAccept();
 
     static EntityID genNewEID();
 
@@ -43,8 +42,25 @@ private:
     /// @brief Broadcaster which spawns up another thread advertising this lobby
     LobbyBroadcaster lobby_broadcaster;
 
+    void _doAccept();
+
     tcp::acceptor acceptor;
     tcp::socket socket;
+
+    /**
+     * Takes the address of an incoming TCP Connection, and handles it by
+     * putting it into the Sessions map, taking care if whether this is a reconnection
+     * or initial connection.
+     * 
+     * Precondition: this->socket holds a socket for a new connection that was just
+     * established by the tcp::acceptor.
+     * 
+     * Postcondition: this->socket is no longer a valid socket as it has been moved
+     * into the Session.
+     * 
+     * @returns a shared_ptr to the Session that was created/reestablished
+     */
+    std::shared_ptr<Session> _handleNewSession(boost::asio::ip::address addr);
 
     /// @brief Mapping from either player id or ip to session
     Sessions sessions;
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -34,7 +34,7 @@ Server::Server(boost::asio::io_context& io_context, GameConfig config)
     Object* obj = state.createObject();
     obj->position = glm::vec3(0.0f, 0.0f, 0.0f);
     
-    doAccept(); // start asynchronously accepting
+    _doAccept(); // start asynchronously accepting
 
     if (config.server.lobby_broadcast) {
         this->lobby_broadcaster.startBroadcasting(ServerLobbyBroadcastPacket {
@@ -67,20 +67,24 @@ std::vector<Event> Server::getAllClientEvents() {
     std::vector<Event> allEvents;
 
     // Loop through each session
-    for (const auto& [eid, session] : this->sessions) {
-        // Get events from the current session
-        std::vector<Event> sessionEvents = session->getEvents();
+    for (const auto& [_eid, _ip, session] : this->sessions) {
+        if (auto s = session.lock()) {
+            // Get events from the current session
+            std::vector<Event> sessionEvents = s->getEvents();
 
-        // Append session events to the overall vector
-        allEvents.insert(allEvents.end(), sessionEvents.begin(), sessionEvents.end());
+            // Append session events to the overall vector
+            allEvents.insert(allEvents.end(), sessionEvents.begin(), sessionEvents.end());
+        }
     }
 
     return allEvents;
 }
 
 void Server::sendUpdateToAllClients(Event event) {
-    for (const auto& [eid, session] : this->sessions) {
-        session->sendEventAsync(event); // SEND UPDATED GAME STATE TO CLIENTS
+    for (const auto& [_eid, _ip, session] : this->sessions) {
+        if (auto s = session.lock()) {
+            s->sendEventAsync(event);
+        }
     }
 }
 
@@ -89,8 +93,7 @@ std::chrono::milliseconds Server::doTick() {
 
     switch (this->state.getPhase()) {
         case GamePhase::LOBBY:
-
-            // Go through sessions and update GameState lobby info
+            // Go through sessions and update GameStte lobby info
             for (const auto& [eid, ip, session]: this->sessions) {
                 if (auto s = session.lock()) {
                     this->state.addPlayerToLobby(eid, s->getInfo().client_name.value_or("UNKNOWN NAME"));
@@ -98,15 +101,6 @@ std::chrono::milliseconds Server::doTick() {
                     this->state.removePlayerFromLobby(eid);
                 }
             }
-
-            // Tell each client the current lobby status
-            for (const auto& [eid, ip, session]: this->sessions) {
-                if (auto s = session.lock()) {
-                    s->sendEventAsync(Event(this->world_eid,
-                        EventType::LoadGameState, LoadGameStateEvent(this->state)));
-                }
-            };
-
             break;
         case GamePhase::GAME: {
             std::vector<Event> allClientEvents = getAllClientEvents();
@@ -121,34 +115,69 @@ std::chrono::milliseconds Server::doTick() {
             std::exit(1);
     }
 
+    // Calculate how long we need to wait until the next tick
     auto stop = std::chrono::high_resolution_clock::now();
-
     auto wait = std::chrono::duration_cast<std::chrono::milliseconds>(
         this->state.getTimestepLength() - (stop - start));
-
     assert(wait.count() > 0);
     return wait;
 }
 
-void Server::doAccept() {
+void Server::_doAccept() {
     this->acceptor.async_accept(this->socket,
         [this](boost::system::error_code ec) {
             if (!ec) {
-                EntityID eid = Server::genNewEID();
-                auto session = std::make_shared<Session>(std::move(this->socket),
-                    SessionInfo({}, eid));
+                auto addr = this->socket.remote_endpoint().address();
+                auto new_session = this->_handleNewSession(addr);
 
-                this->sessions.insert(SessionEntry(eid, 
-                    this->socket.remote_endpoint().address(), session));
-
-                session->startListen();
-
-                session->sendPacketAsync(PackagedPacket::make_shared(PacketType::ServerAssignEID,
-                    ServerAssignEIDPacket { .eid = eid }));
+                new_session->startListen();
+                new_session->sendPacketAsync(PackagedPacket::make_shared(PacketType::ServerAssignEID,
+                    ServerAssignEIDPacket { .eid = new_session->getInfo().client_eid.value() }));
             } else {
                 std::cerr << "Error accepting tcp connection: " << ec << std::endl;
             }
 
-            doAccept();
+            this->_doAccept();
         });
+}
+
+std::shared_ptr<Session> Server::_handleNewSession(boost::asio::ip::address addr) {
+    auto& by_ip = this->sessions.get<IndexByIP>();
+    auto old_session = by_ip.find(addr);
+    if (old_session != by_ip.end()) {
+        // We already had a session with this IP
+        if (old_session->session.expired()) {
+            EntityID old_id = old_session->id;
+
+            // The old session is expired, so create new one
+            auto new_session = std::make_shared<Session>(std::move(this->socket),
+                SessionInfo({}, old_id));
+            by_ip.replace(old_session, SessionEntry(old_id, addr, new_session));
+
+            std::cout << "Reestablished connection with " << addr 
+                << ", which was previously assigned eid " << old_id << std::endl;
+
+        } else {
+            // Some some reason the session is still alive, but we are getting
+            // a connection request from the host?
+            std::cerr << "Error: incoming connection request from " << addr
+                << " with which we already have an active session" << std::endl;
+        }
+    } else {
+        // Brand new connection
+        // TODO: reject connection if not in LOBBY GamePhase
+        EntityID id = Server::genNewEID();
+        auto session = std::make_shared<Session>(std::move(this->socket),
+            SessionInfo({}, id));
+
+        this->sessions.insert(SessionEntry(id, 
+            this->socket.remote_endpoint().address(), session));
+
+        std::cout << "Established new connection with " << addr << ", which was assigned eid "
+            << id << std::endl;
+
+        session->startListen();
+        session->sendPacketAsync(PackagedPacket::make_shared(PacketType::ServerAssignEID,
+            ServerAssignEIDPacket { .eid = id }));
+    }
 }
