73c7a58add2fdbb35247d00975b435d50e7a4a39
Anthony Tarbinian <atar137h@gmail.com>
2024-05-10 22:12:58 -0700

    bbox combining

--- src/client/util.cpp
+++ src/client/util.cpp
@@ -1,7 +1,10 @@
 #include "client/util.hpp"
 
 #include <algorithm>
+#include <limits>
+#include <iostream>
 
+#include "glm/fwd.hpp"
 #include "glm/glm.hpp"
 
 glm::vec3 aiColorToGLM(const aiColor3D& color) {
@@ -9,39 +12,68 @@ glm::vec3 aiColorToGLM(const aiColor3D& color) {
 }
 
 glm::vec3 Bbox::getDimensions() {
-    return this->max - this->min;
-}
-
-bool operator==(const Bbox& bbox1, const Bbox& bbox2) {
-    return (bbox1.min == bbox2.min) && (bbox2.max == bbox2.max);
+    return glm::abs(this->corners.first - this->corners.second);
 }
 
 Bbox aiBboxToGLM(const aiAABB& bbox) {
     return Bbox {
-        .min = glm::vec3(
-            bbox.mMin.x,
-            bbox.mMin.y,
-            bbox.mMin.z
-        ),
-        .max = glm::vec3(
-            bbox.mMax.x,
-            bbox.mMax.y,
-            bbox.mMax.z
-        ),
+        .corners = std::pair<glm::vec3, glm::vec3>(
+            glm::vec3(
+                bbox.mMin.x,
+                bbox.mMin.y,
+                bbox.mMin.z
+            ),
+            glm::vec3(
+                bbox.mMax.x,
+                bbox.mMax.y,
+                bbox.mMax.z
+            )
+        )
     };
 }
 
 Bbox combineBboxes(const Bbox& bbox1, const Bbox& bbox2) {
-    glm::vec3 newMin(
-        std::min(bbox1.min.x, bbox2.min.x),
-        std::min(bbox1.min.y, bbox2.min.y),
-        std::min(bbox1.min.z, bbox2.min.z));
-    glm::vec3 newMax(
-        std::max(bbox1.max.x, bbox2.max.x),
-        std::max(bbox1.max.y, bbox2.max.y),
-        std::max(bbox1.max.z, bbox2.max.z));
+    float minX = std::numeric_limits<float>::infinity();
+    float minY = std::numeric_limits<float>::infinity();
+    float minZ = std::numeric_limits<float>::infinity(); 
+    float maxX = -std::numeric_limits<float>::infinity();
+    float maxY = -std::numeric_limits<float>::infinity();
+    float maxZ = -std::numeric_limits<float>::infinity(); 
+
+    minX = std::min(minX, bbox1.corners.first.x);
+    minX = std::min(minX, bbox1.corners.second.x);
+    minX = std::min(minX, bbox2.corners.first.x);
+    minX = std::min(minX, bbox2.corners.second.x);
+
+    minY = std::min(minY, bbox1.corners.first.y);
+    minY = std::min(minY, bbox1.corners.second.y);
+    minY = std::min(minY, bbox2.corners.first.y);
+    minY = std::min(minY, bbox2.corners.second.y);
+
+    minZ = std::min(minZ, bbox1.corners.first.z);
+    minZ = std::min(minZ, bbox1.corners.second.z);
+    minZ = std::min(minZ, bbox2.corners.first.z);
+    minZ = std::min(minZ, bbox2.corners.second.z);
+
+    maxX = std::max(maxX, bbox1.corners.first.x);
+    maxX = std::max(maxX, bbox1.corners.second.x);
+    maxX = std::max(maxX, bbox2.corners.first.x);
+    maxX = std::max(maxX, bbox2.corners.second.x);
+
+    maxY = std::max(maxY, bbox1.corners.first.y);
+    maxY = std::max(maxY, bbox1.corners.second.y);
+    maxY = std::max(maxY, bbox2.corners.first.y);
+    maxY = std::max(maxY, bbox2.corners.second.y);
+
+    maxZ = std::max(maxZ, bbox1.corners.first.z);
+    maxZ = std::max(maxZ, bbox1.corners.second.z);
+    maxZ = std::max(maxZ, bbox2.corners.first.z);
+    maxZ = std::max(maxZ, bbox2.corners.second.z);
+
     return Bbox{
-        newMin,
-        newMax
+        .corners = std::pair<glm::vec3, glm::vec3>(
+            glm::vec3(minX, minY, minZ),
+            glm::vec3(maxX, maxY, maxZ)
+        )
     };
 }
