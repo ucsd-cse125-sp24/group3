64ad3c9e70ed33d704cb98933fd8bf2b9f1c1dc9
tedpark217 <jhp006@ucsd.edu>
2024-05-15 21:29:28 -0700

    add description of creating item

--- include/server/game/item.hpp
+++ include/server/game/item.hpp
@@ -3,6 +3,17 @@
 #include "server/game/object.hpp"
 #include "server/game/servergamestate.hpp"
 
+/*
+* Things to do when adding items:
+* 1. Make a class / or add onto existing inheritance tree
+* 2. Add to gridcell.hpp / .cpp
+* 3. Create the cell in servergamestate.cpp
+* 4. Create a modeltype for item in sharedmodel.hpp
+* 5. Make client.cpp render the item properly
+* 6. Update img.hpp / .cpp & add the sprite in assets/img
+* 7. Add this item in _sharedGameHUD, gui.cpp, and add text
+*/
+
 /**
  * Class for any possible items that can go in the inventory
  */
