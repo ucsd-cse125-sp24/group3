2a2671f9a93d0c95d76fcfaaffe9c10e1d976186
Anthony Tarbinian <atar137h@gmail.com>
2024-05-29 11:02:53 -0700

    remove debugging __line__s

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -453,7 +453,6 @@ void Client::processServerInput(bool allow_defer) {
 }
 
 void Client::configureGBuffer() {
-    std::cout << __LINE__ << std::endl;
     glGenFramebuffers(1, &gBuffer);
     glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
 
@@ -464,7 +463,6 @@ void Client::configureGBuffer() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, gPosition, 0);
-    std::cout << __LINE__ << std::endl;
     // normal color buffer
     glGenTextures(1, &gNormal);
     glBindTexture(GL_TEXTURE_2D, gNormal);
@@ -472,7 +470,6 @@ void Client::configureGBuffer() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT1, GL_TEXTURE_2D, gNormal, 0);
-    std::cout << __LINE__ << std::endl;
     // color + specular color buffer
     glGenTextures(1, &gAlbedoSpec);
     glBindTexture(GL_TEXTURE_2D, gAlbedoSpec);
@@ -480,12 +477,10 @@ void Client::configureGBuffer() {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT2, GL_TEXTURE_2D, gAlbedoSpec, 0);
-    std::cout << __LINE__ << std::endl;
 
     // tell OpenGL which color attachments we'll use (of this framebuffer) for rendering 
     unsigned int attachments[3] = { GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2 };
     glDrawBuffers(3, attachments);
-    std::cout << __LINE__ << std::endl;
 
     // create and attach depth buffer (renderbuffer)
     unsigned int rboDepth;
@@ -493,18 +488,15 @@ void Client::configureGBuffer() {
     glBindRenderbuffer(GL_RENDERBUFFER, rboDepth);
     glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT, WINDOW_WIDTH, WINDOW_HEIGHT);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, rboDepth);
-    std::cout << __LINE__ << std::endl;
     // finally check if framebuffer is complete
     if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
         std::cout << "Framebuffer not complete!" << std::endl;
     glBindFramebuffer(GL_FRAMEBUFFER, 0);
-    std::cout << __LINE__ << std::endl;
 
     this->deferred_lighting_shader->use();
     this->deferred_lighting_shader->setInt("gPosition", 0);
     this->deferred_lighting_shader->setInt("gNormal", 1);
     this->deferred_lighting_shader->setInt("gAlbedoSpec", 2);
-    std::cout << __LINE__ << std::endl;
 }
 
 void Client::draw() {
